e3f4c1de69e112b887f191e886988093e5e9fe13
Virenbar <rib.artem@gmail.com>
2021-07-06 13:59:42 +0500

    Add more OperTypes

--- PochtaAPI/OperType.cs
+++ PochtaAPI/OperType.cs
@@ -1,23 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace PochtaAPI
+﻿namespace PochtaAPI
 
 {
-	/// <summary>
-	/// Коды операций над отправлениями
-	/// </summary>
-	public enum OperType
-	{
-		/// <summary>
-		/// Прием
-		/// </summary>
-		Sending = 1,
+    /// <summary>
+    /// Коды операций над отправлениями
+    /// </summary>
+    public enum OperType
+    {
+        /// <summary>
+        /// Прием
+        /// </summary>
+        Sending = 1,
 
-		/// <summary>
-		/// Вручение
-		/// </summary>
-		Receiving = 2
-	}
+        /// <summary>
+        /// Вручение
+        /// </summary>
+        Delivery = 2,
+
+        /// <summary>
+        /// Возврат
+        /// </summary>
+        Return = 3,
+
+        /// <summary>
+        /// Досылка почты
+        /// </summary>
+        Receiving = 4,
+
+        /// <summary>
+        /// Невручение
+        /// </summary>
+        NotReceiving = 5,
+
+        /// <summary>
+        /// Хранение
+        /// </summary>
+        Storage = 6,
+
+        /// <summary>
+        /// Временное хранение
+        /// </summary>
+        TempStorage = 7,
+
+        /// <summary>
+        /// Обработка
+        /// </summary>
+        Processing = 8,
+
+        /// <summary>
+        /// Импорт международной почты
+        /// </summary>
+        Import = 9,
+
+        /// <summary>
+        /// Экспорт международной почты
+        /// </summary>
+        Export = 10,
+
+        /// <summary>
+        /// Прием на таможню
+        /// </summary>
+        Customs = 11,
+
+        /// <summary>
+        /// Неудачная попытка вручения
+        /// </summary>
+        FailedDelivery = 12
+    }
 }
